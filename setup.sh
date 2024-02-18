4954fb5930950a8cff82f0a2d8adb1ce0820fdc4
Alejandro Ramos <arhcoder@gmail.com>
2024-02-18 00:46:03 -0600

    A [setup.sh] file added to install all to the bot

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "School Rool Call Bot by @arhcoder"
+
+# 1. Creates .env:
+echo " * Discord Bot Token: "
+read TOKEN
+echo " * Encriptation Password: "
+read KEY
+echo -e "TOKEN=${TOKEN}\nKEY=${KEY}" > .env
+
+# 2. Gets Python:
+sudo apt-get update
+sudo apt-get install -y python
+
+# 3. Installs pip:
+sudo apt-get install -y python-pip
+
+# 4. Gets the dependencies for Python:
+pip install discord python-dotenv selenium
+
+echo "Completed Installation :3"
