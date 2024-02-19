8e9ef5e7bc4dea3d6cc6a3dfefaa22f916254c83
Alejandro Ramos <arhcoder@gmail.com>
2024-02-19 17:24:49 -0600

    First day tests corrections: JSON and Correct Checklist button
    Users saved on json instead of csv; It clicks on the Checklist correct button instead of going to the direct url

--- checklist.py
+++ checklist.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 import os
+import time
 from encrypt import decode
 from dotenv import load_dotenv
 
@@ -14,7 +15,7 @@ def checklist(username, crypt_password, course_id=18):
     
     # Try to go to course url:
     try:
-        course_url = "https://moodle.ccbas.uaa.mx/blocks/autoattend/index.php?course="+str(course_id)
+        course_url = "https://moodle.ccbas.uaa.mx/course/view.php?id="+str(course_id)
         browser.get(course_url)
     except:
         raise ValueError(f"URL {course_url} does not works :c")
@@ -38,13 +39,28 @@ def checklist(username, crypt_password, course_id=18):
     except:
         raise ValueError("Maybe text bars or buttons change id ;:T")
 
-    # Click on submit checklist button:
+    # Click on submit checklist link:
+    try:
+        # Find the <a> element with a <span> containing text starting with "Pase":
+        time.sleep(6)
+        link_elements = browser.find_elements(By.XPATH, '//div[@class="media-body align-self-center"]//a[.//span[starts-with(text(), "Pase")]]')
+
+        if link_elements:
+            link_elements[0].click()
+        else:
+            print("No matching elements found for \"Pase de lista\";:T")
+    except:
+        raise ValueError(f"Incorrect Password for \"{username}\" ;:T")
+    
+    # Click on return submit checklist button:
     try:
         check_button = browser.find_element(By.NAME, "submit")
         check_button.click()
     except:
-        raise ValueError(f"Incorrect Password for \"{username}\" ;:T")
+        raise ValueError(f"Maybe wrong page, not checklist ;:T")
 
     # Close the browser:
     browser.quit()
     return True
+
+checklist("al243681", "+\u0007\u0005\u0012*\u001a\u001a\u000b\fYG")
--- users.py
+++ users.py
@@ -1,5 +1,5 @@
 import os
-import csv
+import json
 from dotenv import load_dotenv
 from encrypt import encode
 
@@ -12,44 +12,37 @@ KEY = os.getenv("KEY")
 def save_user(userid, username, password):
     # Encodes the password to save it:
     password = encode(password, KEY)
-    # Load existing users from the CSV file:
+    
+    # Load existing users from the JSON file:
     existing_users = []
     try:
-        with open("users.csv", "r", newline="") as file:
-            reader = csv.reader(file)
-            existing_users = list(reader)
+        with open("users.json", "r") as file:
+            existing_users = json.load(file)
     except FileNotFoundError:
         pass
 
     # Check if userid already exists:
-    user_exists = any(user[0] == userid for user in existing_users)
-    # If it exists, updates the row:
+    user_exists = any(user["userid"] == userid for user in existing_users)
+    # If it exists, updates the user:
     if user_exists:
         for user in existing_users:
-            if user[0] == userid:
-                user[1] = username
-                user[2] = password
+            if user["userid"] == userid:
+                user["username"] = username
+                user["password"] = password
                 break
-    # If not, it appends:
+    # If not, appends a new user:
     else:
-        existing_users.append([userid, username, password])
+        existing_users.append({"userid": userid, "username": username, "password": password})
 
-    # Write the updated/added users back to the CSV file:
-    with open("users.csv", "w", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerows(existing_users)
+    # Write the updated/added users back to the JSON file:
+    with open("users.json", "w") as file:
+        json.dump(existing_users, file)
 
-def get_users(filename="users.csv"):
+def get_users(filename="users.json"):
     users = []
     try:
-        with open(filename, "r", newline="") as file:
-            reader = csv.reader(file)
-            for row in reader:
-                users.append({
-                    "userid": row[0],
-                    "username": row[1],
-                    "password": row[2]
-                })
+        with open(filename, "r") as file:
+            users = json.load(file)
     except FileNotFoundError:
         print(f"File \"{filename}\" not found.")
     return users
